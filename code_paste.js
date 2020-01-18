65482b8cb94c1284d26a8add21f238fdc6e99db7
masaks <oicwam.orz1228@gmail.com>
2020-01-19 05:05:47 +0900

    Docコメントで装飾してみた

--- code_paste.js
+++ code_paste.js
@@ -1,9 +1,20 @@
+/**
+ * エディターのセッション
+ */
 let thisEditor;
+
+/**
+ * Editorの各オプションとコピーボタン
+ * @type {Element}
+ */
 const themeSelect = document.querySelector('#options__select-theme__select');
 const languageSelect = document.querySelector('#options__select-language__select');
 const tabSizeSelect = document.querySelector('#options__tab-size__select');
 const copyButton = document.querySelector('.options__copy-button');
 
+/**
+ * 初期表示時の処理
+ */
 window.onload = () => {
     thisEditor = ace.edit('editor', {
         mode: 'ace/mode/javascript',
@@ -12,23 +23,41 @@ window.onload = () => {
     });
 };
 
+/**
+ * 各オプションのイベント
+ */
 themeSelect.addEventListener('change', ({ target }) => changeTheme(target.value));
 languageSelect.addEventListener('change', ({ target }) => changeLanguagesMode(target.value));
 tabSizeSelect.addEventListener('change', ({target}) => changeTabSize(target.value));
 copyButton.addEventListener('click', () => copyCode());
 
+/**
+ * Editorのテーマを変更する。
+ * @param {string} theme
+ */
 function changeTheme(theme) {
     thisEditor.setTheme(`ace/theme/${theme}`);
 }
 
+/**
+ * 言語モードを変更する。
+ * @param {string} languageMode
+ */
 function changeLanguagesMode(languageMode) {
     thisEditor.session.setMode(`ace/mode/${languageMode}`);
 }
 
+/**
+ * タブサイズを変更する。
+ * @param {string} size
+ */
 function changeTabSize(size) {
-    thisEditor.session.setTabSize(size);
+    thisEditor.session.setTabSize(Number(size));
 }
 
+/**
+ * コードをクリップボードにコピーする。
+ */
 function copyCode() {
     thisEditor.selectAll();
     document.execCommand('copy');
